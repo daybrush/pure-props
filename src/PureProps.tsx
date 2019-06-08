2cdef37c9c5efbe4fddf4238dc44c6a5242fef7e
Daybrush <daybrush@gmail.com>
2019-06-09 02:02:58 +0900

    fix: fix compare order

--- src/PureProps.tsx
+++ src/PureProps.tsx
@@ -4,6 +4,6 @@ import equals from "shallowequal";
 
 export default class PureProps<T = {}, U = {}> extends React.Component<T, U> {
     public shouldComponentUpdate(prevProps: any, prevState: any) {
-        return !equals(prevProps, this.props) || prevState !== this.state;
+        return prevState !== this.state || !equals(prevProps, this.props);
     }
 }
