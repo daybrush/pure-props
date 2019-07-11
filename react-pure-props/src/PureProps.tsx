a3b729ee68f5e3c26a593651b038996c8e9f6b7e
YOUN KUE CHOI <daybrush@gmail.com>
2019-07-11 14:00:53 +0900

    fix: Move react-pure-props path

--- react-pure-props/src/PureProps.tsx
+++ react-pure-props/src/PureProps.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import { Component } from "react";
 import equals from "shallowequal";
 
 
-export default class PureProps<T = {}, U = {}> extends React.Component<T, U> {
+export default class PureProps<T = {}, U = {}> extends Component<T, U> {
     public shouldComponentUpdate(prevProps: any, prevState: any) {
         return prevState !== this.state || !equals(prevProps, this.props);
     }
